6ab064bf97092d562719b15462336bedd7a0af8c
Element <estore.om@gmail.com>
2019-04-23 10:49:16 -0600

    added print route to family

--- routes/family.js
+++ routes/family.js
@@ -230,6 +230,23 @@ router.get('/orphans', verifyToken, async (req, res) => {
 });
 
 // get specific data points needed for familyMember
+router.get('/print/:formId', verifyToken, async (req, res) => {
+  // await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+  // if (err) {
+  // res.sendStatus(403);
+  // } else {
+  try {
+    let result = await Family.find({'formId': req.params['formId']});
+    return res.status(200).json({data: result});
+  }
+  catch(err) {
+    res.status(500).json({message: 'Error in GET family route'});
+  }
+  // }
+  // });
+});
+
+// get specific data points needed for familyMember
 router.get('/:formId', verifyToken, async (req, res) => {
   await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
     if (err) {
