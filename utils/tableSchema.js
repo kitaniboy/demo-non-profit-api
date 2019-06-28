862c16712786563637bcfa90fb6c9769a37eb9c0
Element <estore.om@gmail.com>
2019-06-28 01:37:57 -0600

    trying to remove table data to its own folder to ease usability

---
+++ utils/tableSchema.js
@@ -0,0 +1,14 @@
+let tableSchema = {
+  assistance: [
+    'cost',
+    'familyId',
+    'supportNeeded.date',
+    'supportNeeded.description',
+    'assistanceCategory',
+    'assistanceId',
+    'familyId',
+    '_id'
+  ]
+};
+
+module.export = tableSchema;
