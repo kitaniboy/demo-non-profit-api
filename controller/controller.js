7b11ff933ef6a29f41ccdfb2466199671ba1207b
Ma7eer <maher.alkendi2@gmail.com>
2019-07-26 12:01:05 -0700

    moved assistance to conntroller file

--- controller/controller.js
+++ controller/controller.js
@@ -1,88 +1,84 @@
 const jwt = require('jsonwebtoken')
 const createNewDocument = require('../utils/createNewDoc')
 
-const getOne = (req, res, Model, findOneObject) => {
-    await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
-        if (err) {
-          /* client side should view all 403 as an auth error and deliver
+const getOne = async (req, res, Model, findOneObject) => {
+  await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+    if (err) {
+      /* client side should view all 403 as an auth error and deliver
           the proper message to the user */
-          res.sendStatus(403);
-        } else {
-          try {
-            let result = await Model.findOne(findOneObject);
-            return res.status(200).json({data: result});
-          }
-          catch(err) {
-            res.status(500).json({message: err});
-          }
-        }
-      });
+      res.sendStatus(403)
+    } else {
+      try {
+        let result = await Model.findOne(findOneObject)
+        return res.status(200).json({ data: result })
+      } catch (err) {
+        res.status(500).json({ message: err })
+      }
+    }
+  })
 }
 
-const getAll = (req, res, Model, findAllObject) => {
-    await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
-        if (err) {
-          /* client side should view all 403 as an auth error and deliver
+const getAll = async (req, res, Model, findAllObject) => {
+  await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+    if (err) {
+      /* client side should view all 403 as an auth error and deliver
           the proper message to the user */
-          res.sendStatus(403)
-        } else {
-          try {
-            let result = await Model.find(
-              {},
-              findAllObject
-            )
-            return res.status(200).json({ data: result })
-          } catch (err) {
-            res.status(500).json({ message: err })
-          }
-        }
-      })
+      res.sendStatus(403)
+    } else {
+      try {
+        let result = await Model.find({}, findAllObject)
+        return res.status(200).json({ data: result })
+      } catch (err) {
+        res.status(500).json({ message: err })
+      }
+    }
+  })
 }
 
-const post = (req, res, Model) => {
-    await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
-        if (err) {
-          /* client side should view all 403 as an auth error and deliver
+const post = async (req, res, Model) => {
+  await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+    if (err) {
+      /* client side should view all 403 as an auth error and deliver
           the proper message to the user */
-          res.sendStatus(403)
-        } else {
-          let model = new Model(createNewDocument(Model.schema.obj, req.body))
-          try {
-            await model.save()
-            return res.status(201).json({ message: 'new document created!' })
-          } catch (err) {
-            res.status(500).json({ message: err })
-          }
-        }
-      })
+      res.sendStatus(403)
+    } else {
+      let model = new Model(createNewDocument(Model.schema.obj, req.body))
+      try {
+        await model.save()
+        return res.status(201).json({ message: 'new document created!' })
+      } catch (err) {
+        res.status(500).json({ message: err })
+      }
+    }
+  })
 }
 
-const patch = (req, res, Model) => {
-    await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
-        if (err) {
-          /* client side should view all 403 as an auth error and deliver
+const patch = async (req, res, Model) => {
+  await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+    if (err) {
+      /* client side should view all 403 as an auth error and deliver
           the proper message to the user */
-          res.sendStatus(403)
-        } else {
-          try {
-            await Model.findByIdAndUpdate(
-              { _id: req.params.id },
-              { $set: req.body }
-            )
-            return res.status(200).json({ message: 'existing document updated!' })
-          } catch (err) {
-            res.status(500).json({ message: err })
-          }
-        }
-      })
+      res.sendStatus(403)
+    } else {
+      try {
+        await Model.findByIdAndUpdate(
+          { _id: req.params.id },
+          { $set: req.body }
+        )
+        return res.status(200).json({ message: 'existing document updated!' })
+      } catch (err) {
+        res.status(500).json({ message: err })
+      }
+    }
+  })
 }
 
-const deleteOne = (req, res, Model) => {
+const deleteOne = async (req, res, Model) => {
   /* No authentication is included because delete route does not
   send back a response body so no way to check using jwt. in this
   case authentication is left to the frontend
   */
- try {
+  try {
     await Model.findByIdAndDelete({ _id: req.params.id })
     return res.status(200).json({ message: 'existing document deleted!' })
   } catch (err) {
@@ -91,9 +87,9 @@ const deleteOne = (req, res, Model) => {
 }
 
 module.exports = {
-    getOne,
-    getAll,
-    post,
-    patch,
-    deleteOne
-  }
+  getOne,
+  getAll,
+  post,
+  patch,
+  deleteOne
+}
