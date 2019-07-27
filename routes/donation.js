400e9d397ccf8f1db0cdadf9dd4c1956a50ec637
Ma7eer <maher.alkendi2@gmail.com>
2019-07-27 10:28:34 -0700

    fix

--- routes/donation.js
+++ routes/donation.js
@@ -35,7 +35,10 @@ router.get('/', verifyToken, async (req, res) => {
       res.sendStatus(403)
     } else {
       try {
-        let result = await Model.find({}, clientSideTableData.borrow.join(' '))
+        let result = await Model.find(
+          {},
+          clientSideTableData.assistance.join(' ')
+        )
         return res.status(200).json({ data: result })
       } catch (err) {
         res.status(500).json({ message: err })
