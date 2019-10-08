9836e3a73d669a5aecf0f7bc29ed7a3ba4fd1403
Ma7eer <maher.alkendi2@gmail.com>
2019-10-08 11:15:15 -0700

    fix bug

--- routes/orphans/orphans.js
+++ routes/orphans/orphans.js
@@ -23,6 +23,21 @@ router.get('/:id', verifyToken, async (req, res) => {
   //     res.sendStatus(403);
   //   } else {
   try {
+    let result = await Model.find({ orphanFamilyId: req.params.id })
+    return res.status(200).json({ data: result })
+  } catch (err) {
+    res.status(500).json({ message: 'Error in GET LowIncomePayments route' })
+  }
+  // }
+  // });
+})
+
+router.get('/getOne/:id', verifyToken, async (req, res) => {
+  // await jwt.verify(req.token, 'alrahmasecrestkey', async (err, authData) => {
+  //   if (err) {
+  //     res.sendStatus(403);
+  //   } else {
+  try {
     let result = await Model.find({ orphanId: req.params.id })
     return res.status(200).json({ data: result })
   } catch (err) {
