8141502c0aeda446d506ff57c2d229df20c63ac0
Ma7eer <maher.alkendi2@gmail.com>
2019-10-26 11:18:05 -0700

    cda

--- models/productiveFamilies/productiveFamilies.js
+++ models/productiveFamilies/productiveFamilies.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+/*
+TRANSLATION
+==============================================
+ formId: رقم الارشفة
+ nameOfRecipient: اسم الباحث عن عمل 
+ dateOfContact: تاريخ التواصل 
+ formOfContact: طريقة التواصل
+ relationship:علاقة المستفيد بالاسم الرمجعي
+ ageOfRecipient:العمر 
+ notes:ملاحظات 
+ fieldOfWork:المجال- كفاف 
+ recipientEducationLevel:المستوى الدراسي 
+ recipientNationalId:الرقم المدني 
+ reasonForNotRegistering: سبب عدم الرغبة في التسجيل /كفاف
+ phoneNumberOfRecipient:رقم الهاتف 
+ isRegistered: كفاف ( يرغب/لا يرغب)
+ reasonForContact: تفاصيل التواصل 
+  status:حالة الفرد 
+  workExperience:خبرات العمل
+==============================================
+*/
+
 const productiveFamilySchema = new Schema(
   {
     id: { type: Schema.Types.ObjectId },
@@ -8,12 +30,17 @@ const productiveFamilySchema = new Schema(
     nameOfRecipient: { type: String, default: '' },
     relationship: { type: String, default: '' },
     ageOfRecipient: { type: String, default: '' },
-    RecipientNationalId: { type: String, default: '' },
-    RecipientEducationLevel: { type: String, default: '' },
+    recipientNationalId: { type: String, default: '' },
+    recipientEducationLevel: { type: String, default: '' },
     fieldOfWork: { type: String, default: '' },
     formOfContact: { type: String, default: '' },
     dateOfContact: { type: String, default: '' },
-    reasonForNotRegistering: { type: String, default: '' }
+    reasonForNotRegistering: { type: String, default: '' },
+    phoneNumberOfRecipient: { type: String, default: '' },
+    isRegistered: { type: String, default: '' },
+    reasonForContact: { type: String, default: '' },
+    status: { type: String, default: '' },
+    workExperience: { type: String, default: '' }
   }
   //   {
   //     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
