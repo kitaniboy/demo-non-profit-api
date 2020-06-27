a3d5c5531acb2c7356e394b47c0ab4aec76cc8e0
Ma7eer <maher.alkendi2@gmail.com>
2020-06-27 14:03:40 +0400

    added isproductivefam member to fam member model

--- models/Archives/familyMembers.js
+++ models/Archives/familyMembers.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const FamilyMembersSchema = new Schema({
-  id: { type: Schema.Types.ObjectId },
-  familyMemberId: { type: String, default: ''},
-  familyId: { type: String, default: '' },
-  familyMemberName: { type: String, default: '' },
-  relation: { type: String, default: '' },
-  socialStatus:  { type: String, default: '' },
-  dateOfBirth: { type: String, default: ''},
-  educationLevel:   { type: String, default: '' },
-  job: { type: String, default: '' },
-  workPlace: { type: String, default: '' },
-  familyMemberSalary: { type: String, default: '' },
-  familyMemberLoan: { type: String, default: '' },
-  monthlyInstallment: { type: String, default: '' },
-  loanReason: { type: String, default: '' },
-  health: { type: String, default: '' },
-  retirementSalary: { type: String, default: '' },
-  age: { type: String, default: '' }
-}, {timestamps: { createdAt: 'created_at', updatedAt: 'updated_at'}});
+const FamilyMembersSchema = new Schema(
+  {
+    id: { type: Schema.Types.ObjectId },
+    familyMemberId: { type: String, default: '' },
+    familyId: { type: String, default: '' },
+    familyMemberName: { type: String, default: '' },
+    relation: { type: String, default: '' },
+    socialStatus: { type: String, default: '' },
+    dateOfBirth: { type: String, default: '' },
+    educationLevel: { type: String, default: '' },
+    job: { type: String, default: '' },
+    workPlace: { type: String, default: '' },
+    familyMemberSalary: { type: String, default: '' },
+    familyMemberLoan: { type: String, default: '' },
+    monthlyInstallment: { type: String, default: '' },
+    loanReason: { type: String, default: '' },
+    health: { type: String, default: '' },
+    retirementSalary: { type: String, default: '' },
+    age: { type: String, default: '' },
+    isProductiveFamilyMember: { type: Boolean, default: false }
+  },
+  { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }
+)
 
-module.exports = mongoose.model('FamilyMembers', FamilyMembersSchema);
+module.exports = mongoose.model('FamilyMembers', FamilyMembersSchema)
